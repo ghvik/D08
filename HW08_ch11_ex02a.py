3eeca19304453e2af67d7e763062229e3baa9c23
ghvik <vreddy@ischool.berkeley.edu>
2016-08-12 19:16:08 -0700

    Finished ex02, word count, need to fix punctuation though

--- HW08_ch11_ex02a.py
+++ HW08_ch11_ex02a.py
@@ -37,16 +37,21 @@ def histogram_old(s):
 
 
 def histogram_new(s):
-    ...
-
+    d = dict()
+    for word in s:
+        d[word] = d.get(word, 0) + 1
+    return d
 
 def get_pledge_list():
     """ Opens pledge.txt and converts to a list, each item is a word in
     the order it appears in the original file. returns the list.
     """
-    # Your code here.
-    pass
-    # return pledge_list (uncomment this)
+    pledge_list = []
+    with open("pledge.txt", "r") as fin:
+        for line in fin:
+            for word in line.split():
+                pledge_list.append(word)
+    return pledge_list
 
 
 ###############################################################################
